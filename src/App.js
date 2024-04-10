7b0eb868c19d9514a3123d9d529ca3c1e5e44774
eunbin <dmsqlsdl54@gmail.com>
2024-04-10 22:24:20 +0900

    정렬 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import FoodList from "./components/FoodList/FoodList";
 import items from "./mock.json";
 
 const App = (props) => {
+  // 정렬 기능
+  const [order, setOrder] = useState("createdAt");
+  const handleNewestOrder = () => setOrder("createdAt");
+  const handleCalorieOrder = () => setOrder("calorie");
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]); // 정렬된 아이템
+
   return (
     <div>
-      <FoodList items={items} />
+      <button onClick={handleNewestOrder}>최신순</button>
+      <button onClick={handleCalorieOrder}>칼로리순</button>
+      <FoodList items={sortedItems} />
     </div>
   );
 };
